742d5aceebf92e66f8e421bebe30dbd0f8c39da9
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-02-05 23:26:53 +0200

    Adding documentation to the MemoryCard file

--- MemoryCard.cs
+++ MemoryCard.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace MemoryGame
 {
+    /// <summary>Represents the letter sign displayed on a memory card (A through R).</summary>
     public enum eCardSign
     {
         A = 'A',
@@ -28,17 +29,26 @@ namespace MemoryGame
         R = 'R'
     }
 
+    /// <summary>
+    /// Represents a single card in the Memory game. Each card has a sign and can be revealed or hidden.
+    /// </summary>
     public struct MemoryCard
     {
         private readonly eCardSign r_CardSign;
         private bool m_IsCardRevealed;
         
+        /// <summary>
+        /// Initializes a new card with the specified sign and visibility state.
+        /// </summary>
+        /// <param name="i_CardSign">The letter sign displayed when the card is revealed.</param>
+        /// <param name="i_IsCardRevealed">True if the card is face-up; false if face-down.</param>
         public MemoryCard(eCardSign i_CardSign, bool i_IsCardRevealed)
         {
             r_CardSign = i_CardSign;
             m_IsCardRevealed = i_IsCardRevealed;
         }
         
+        /// <summary>Gets the letter sign displayed when the card is revealed.</summary>
         public eCardSign CardSign
         {
             get
@@ -47,6 +57,7 @@ namespace MemoryGame
             }
         }
         
+        /// <summary>Gets whether the card is currently face-up (revealed) or face-down (hidden).</summary>
         public bool IsCardRevealed
         {
             get
@@ -55,6 +66,7 @@ namespace MemoryGame
             }
         }
         
+        /// <summary>Toggles the card between revealed and hidden states.</summary>
         public void FlipCard()
         {
             m_IsCardRevealed = !m_IsCardRevealed;
